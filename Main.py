9ae38f188aee94f37572726bb736e41e46c609f7
M1ch777 <105068997+M1ch777@users.noreply.github.com>
2022-07-27 10:16:13 -0500

    Add files via upload

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ from IMC_Clases import *
 import pymongo
 
 if __name__== '__main__':
-    '''Creación de la base de datos BaseDataIMC y la colección Usuarios en MongoDB'''
+    
     myClient = pymongo.MongoClient("mongodb://localhost:27017/")
     MONGO_BASED = "BaseDataIMC"
     COLECCION = "Usuarios"
